b5c8b47f5d3beff6857f5aa74c0803ac83b0c7a9
EuiHun_Bori <euihunbaek@gmail.com>
2025-04-15 23:02:42 +0900

    Update ResponseBodyController.java

--- src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
+++ src/main/java/hello/springmvc/basic/response/ResponseBodyController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+
 @Slf4j
 @Controller
 public class ResponseBodyController {
